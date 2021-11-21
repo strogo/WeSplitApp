5821e256edc927e53256b1e0018b226a6131a7f1
pavelsurovy <surolm@centrum.sk>
2021-11-21 09:07:38 +0100

    Initial Commit

---
+++ WeSplitApp/WeSplitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitAppApp.swift
+//  WeSplitApp
+//
+//  Created by Palino on 21/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
